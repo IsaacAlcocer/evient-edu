0b0354876ee64e30bf2d0edb5c1e59e1a5a03d8b
Karla Rocha <leonoemi123@gmail.com>
2023-04-12 12:22:41 -0700

    Check if any rows are winners

--- src/index.js
+++ src/index.js
@@ -6,7 +6,33 @@ let currentSymbol = "X"
 function onTileClick(col, row) {
   let tile = document.getElementById(`btn-${col}-${row}`)
   tile.innerHTML = currentSymbol;
+  checkIfUserIsWinner(currentSymbol);
   changeSymbol();
+}
+
+function checkIfUserIsWinner(cSymbol) {
+  let rowWinners = 0;
+  for (let i = 0; i < ROWS; i++) {
+    const rowIsWinner = checkIfRowIsWinner(cSymbol, i);
+    if (rowIsWinner) {
+      rowWinners++;
+    }
+  }
+  if (rowWinners !== 0) {
+    alert(`${cSymbol} is the Winner!`)
+  }
+  // Are any columns winners?
+}
+
+function checkIfRowIsWinner(cSymbol, row) {
+  let symbolCount = 0;
+  for (let j = 0; j < COLUMNS; j++) {
+    let tile = document.getElementById(`btn-${row}-${j}`)
+    if (cSymbol === tile.innerText) {
+      symbolCount++;
+    }
+  }
+  return symbolCount === COLUMNS;
 }
 
 function changeSymbol() {
