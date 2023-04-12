59942b43cc6a9a74569223447ac4b6556b0952f2
Karla Rocha <leonoemi123@gmail.com>
2023-04-12 15:46:46 -0700

    Solution to tic-tac-toe homework

--- src/index.js
+++ src/index.js
@@ -11,17 +11,59 @@ function onTileClick(col, row) {
 }
 
 function checkIfUserIsWinner(cSymbol) {
-  let rowWinners = 0;
+  let rowWinners = 0, columnWinners = 0;
   for (let i = 0; i < ROWS; i++) {
     const rowIsWinner = checkIfRowIsWinner(cSymbol, i);
     if (rowIsWinner) {
       rowWinners++;
     }
   }
-  if (rowWinners !== 0) {
-    alert(`${cSymbol} is the Winner!`)
+  for (let j = 0; j < COLUMNS; j++) {
+    const columnIsWinner = checkIfColumnIsWinner(cSymbol, j);
+    if (columnIsWinner) {
+      columnWinners++;
+    }
   }
-  // Are any columns winners?
+
+  const isWinner = rowWinners !== 0 || columnWinners !== 0 || checkIfAscDiagonalIsWinner(cSymbol) || checkIfDescDiagonalIsWinner(cSymbol);
+  if (isWinner) {
+    alert(`${cSymbol} is the Winner!`);
+    resetGame();
+  }
+  
+}
+
+function checkIfDescDiagonalIsWinner(cSymbol) {
+  let symbolCount = 0;
+  for (let i = 0; i < COLUMNS; i++) {
+    let tile = document.getElementById(`btn-${i}-${i}`);
+    if (cSymbol === tile.innerText) {
+      symbolCount++;
+    }
+  }
+  return symbolCount === COLUMNS;
+}
+
+function checkIfAscDiagonalIsWinner(cSymbol) {
+  let symbolCount = 0;
+  for (let i = 0, j = ROWS - 1; i < COLUMNS; i++, j--) {
+    let tile = document.getElementById(`btn-${i}-${j}`);
+    if (cSymbol === tile.innerText) {
+      symbolCount++;
+    }
+  }
+  return symbolCount === COLUMNS;
+}
+
+function checkIfColumnIsWinner(cSymbol, column) {
+  let symbolCount = 0;
+  for (let i = 0; i < ROWS; i++) {
+    let tile = document.getElementById(`btn-${i}-${column}`);
+    if (cSymbol === tile.innerText) {
+      symbolCount++;
+    }
+  }
+  return symbolCount === ROWS;
 }
 
 function checkIfRowIsWinner(cSymbol, row) {
@@ -46,15 +88,25 @@ function createBoard() {
 
   for (let i = 0; i < ROWS; i++) {
     for (let j = 0; j < COLUMNS; j++) {
-      const tile = document.createElement("div");
+      let tile = document.createElement("div");
       tile.id = `btn-${i}-${j}`;
       tile.className = "tile";
-      tile.innerHTML = `${i}${j}`;
       tile.onclick = () => onTileClick(i, j);
       board.appendChild(tile)
     }
   }
   document.body.appendChild(board);
+}
+
+function resetGame() {
+  currentSymbol = "X";
+
+  for (let i = 0; i < ROWS; i++) {
+    for (let j = 0; j < COLUMNS; j++) {
+      let tile = document.getElementById(`btn-${i}-${j}`);
+      tile.innerText = "";
+    }
+  }
 }
 
 createBoard();
