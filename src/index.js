0f35350afd75f54eb467fbfe70367c0732425290
Karla Rocha <leonoemi123@gmail.com>
2023-04-18 13:19:21 -0700

    :necktie: Added a practice problem

--- src/index.js
+++ src/index.js
@@ -1,60 +1,25 @@
-import './style.css';
+import {problem1Solution} from './problem1';
 
-const COLUMNS = 3, ROWS = 3;
-let currentSymbol = "X"
-
-function onTileClick(col, row) {
-  let tile = document.getElementById(`btn-${col}-${row}`)
-  tile.innerHTML = currentSymbol;
-  checkIfUserIsWinner(currentSymbol);
-  changeSymbol();
+function getRandomArbitrary(min, max) {
+  return parseInt(Math.random() * (max - min) + min);
 }
 
-function checkIfUserIsWinner(cSymbol) {
-  let rowWinners = 0;
-  for (let i = 0; i < ROWS; i++) {
-    const rowIsWinner = checkIfRowIsWinner(cSymbol, i);
-    if (rowIsWinner) {
-      rowWinners++;
-    }
-  }
-  if (rowWinners !== 0) {
-    alert(`${cSymbol} is the Winner!`)
-  }
-  // Are any columns winners?
+function getRandomInt(max) {
+  return Math.floor(Math.random() * max);
 }
 
-function checkIfRowIsWinner(cSymbol, row) {
-  let symbolCount = 0;
-  for (let j = 0; j < COLUMNS; j++) {
-    let tile = document.getElementById(`btn-${row}-${j}`)
-    if (cSymbol === tile.innerText) {
-      symbolCount++;
-    }
-  }
-  return symbolCount === COLUMNS;
+function render(){
+  const size = getRandomArbitrary(5, 10);
+  const numList = Array.from({length: size}, () => getRandomInt(30));
+  const numListText = `[${numList.join(", ")}] = ?`;
+  const textDisplay = document.createElement('div');
+  textDisplay.innerText = numListText;
+  textDisplay.className = "text-display";
+  const answerDisplay = document.createElement('div');
+  answerDisplay.innerText = problem1Solution(numList);
+  answerDisplay.className = "answer-display";
+  document.body.appendChild(textDisplay);
+  document.body.appendChild(answerDisplay);
 }
 
-function changeSymbol() {
-  if (currentSymbol === "X") currentSymbol = "O";
-  else currentSymbol = "X";
-}
-
-function createBoard() {
-  const board = document.createElement("div");
-  board.className = "board-container";
-
-  for (let i = 0; i < ROWS; i++) {
-    for (let j = 0; j < COLUMNS; j++) {
-      const tile = document.createElement("div");
-      tile.id = `btn-${i}-${j}`;
-      tile.className = "tile";
-      tile.innerHTML = `${i}${j}`;
-      tile.onclick = () => onTileClick(i, j);
-      board.appendChild(tile)
-    }
-  }
-  document.body.appendChild(board);
-}
-
-createBoard();
+render();
