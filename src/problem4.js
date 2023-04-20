810f6c00bee50b6851c77ee21a30488015572f07
Karla Rocha <leonoemi123@gmail.com>
2023-04-20 13:22:47 -0700

    :necktie: Added a 4th problem

---
+++ src/problem4.js
@@ -0,0 +1,19 @@
+/**
+ * 
+ * @param number[][] matrix
+ * @returns string
+ * 
+ * Ejercicio 4
+ * La función 'problema4Solution' va recibir un parametro llamad 'matrix'.
+ * El parametro 'matrix' es un arreglo (o lista) dentro de otro arreglo.
+ * En cada celda puede conter un 'Red' o 'Blue'.
+ * 
+ * El objetivo de este ejercicio es determinar cuando cuando hay de tipo 'Red'
+ * y cuando hay de tipo 'Blue'.
+ *
+ */
+
+
+export function problem4Solution(matrix) {
+    return `Red: ${0}, Blue: ${0}`;
+}
