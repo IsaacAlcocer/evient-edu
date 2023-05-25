d919c1a1b376f26872d09a0d5d5c2a447bd6a16c
Karla Rocha <leonoemi123@gmail.com>
2023-05-25 09:11:49 -0700

    Added html plugin for webpack & updated webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,13 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  entry: './src/index.js',
+  entry: {
+    main: './src/index.js',
+    createPlayer: './src/createPlayer.js',
+  },
   output: {
-    filename: 'main.js',
+    filename: '[name].js',
     path: path.resolve(__dirname, 'dist'),
   },
   module: {
@@ -14,4 +18,16 @@ module.exports = {
       },
     ],
   },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+      filename: 'index.html',
+      chunks: ['index'],
+    }),
+    new HtmlWebpackPlugin({
+      template: './src/create-player.html',
+      filename: 'create-player.html',
+      chunks: ['create-player'],
+    }),
+  ],
 };
