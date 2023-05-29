14dff1ab44ba76b441864ca7e6a497760d32fe5f
Karla Rocha <leonoemi123@gmail.com>
2023-05-29 14:30:57 -0700

    More Array problems

---
+++ src/problem3C.js
@@ -0,0 +1,19 @@
+/**
+ * 
+ * @param number[] numList
+ * @returns number
+ * 
+ * Ejercicio 1
+ * La función 'problema3Solution' va recibir un parametro llamad 'numList'.
+ * El parametro 'numList' es un arreglo (o lista) de numeros aleatorios.
+ * 
+ * El objetivo de este ejercicio es encontrar el número más grande regresar
+ * ese valor con el return usando el REDUCE.
+ *
+ */
+
+
+export function problem3CSolution(numList) {
+    return -1;
+}
+
---
+++ src/problem3b.js
@@ -0,0 +1,19 @@
+/**
+ * 
+ * @param number[] numList
+ * @returns number
+ * 
+ * Ejercicio 1 B
+ * La función 'problema3Solution' va recibir un parametro llamad 'numList'.
+ * El parametro 'numList' es un arreglo (o lista) de numeros aleatorios.
+ * 
+ * El objetivo de este ejercicio es encontrar el número más grande regresar
+ * ese valor con el return usando MAP.
+ *
+ */
+
+
+export function problem3BSolution(numList) {
+    return -1;
+}
+
