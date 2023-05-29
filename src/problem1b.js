2b9276eb99edcf02aa0eb40547791575729efc73
Karla Rocha <leonoemi123@gmail.com>
2023-05-29 14:22:21 -0700

    Added more problems for arrays

---
+++ src/problem1b.js
@@ -0,0 +1,18 @@
+/**
+ * 
+ * @param number[] numList
+ * @returns number
+ * 
+ * Ejercicio 1 B
+ * La función 'problema1Solution' va recibir un parametro llamad 'numList'.
+ * El parametro 'numList' es un arreglo (o lista) de numeros.
+ * 
+ * El objetivo de este ejercicio es que regreses la suma de todos estos
+ * numeros usando el método de map de las listas.
+ *
+ */
+
+
+export function problem1BSolution(numList) {
+  return -1;
+}
---
+++ src/problem1c.js
@@ -0,0 +1,18 @@
+/**
+ * 
+ * @param number[] numList
+ * @returns number
+ * 
+ * Ejercicio 1 C
+ * La función 'problema1Solution' va recibir un parametro llamad 'numList'.
+ * El parametro 'numList' es un arreglo (o lista) de numeros.
+ * 
+ * El objetivo de este ejercicio es que regreses la suma de todos estos
+ * numeros usando el método de reduce de las listas.
+ *
+ */
+
+
+export function problem1CSolution(numList) {
+  return -1;
+}
